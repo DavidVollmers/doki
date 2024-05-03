4d71806e51d50b5bb889d0fd5f87e7c6f2c8ec3f
dvolper <github@david.vollmers.org>
2024-05-03 20:42:01 +0200

    Doki.Output.ClassLibrary concept

---
+++ src/Doki.Output.ClassLibrary/ClassLibraryOutput.cs
@@ -0,0 +1,16 @@
+﻿namespace Doki.Output.ClassLibrary;
+
+[DokiOutput("Doki.Output.ClassLibrary")]
+public sealed class ClassLibraryOutput(OutputContext context) : OutputBase<ClassLibraryOutputOptions>(context)
+{
+    public override async Task WriteAsync(ContentList contentList, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(contentList);
+    }
+
+    public override async Task WriteAsync(TypeDocumentation typeDocumentation,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(typeDocumentation);
+    }
+}
---
+++ src/Doki.Output.ClassLibrary/ClassLibraryOutputOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Doki.Output.ClassLibrary;
+
+public sealed record ClassLibraryOutputOptions : OutputOptions
+{
+    [JsonPropertyName("namespace")] public string Namespace { get; init; }
+
+    [JsonPropertyName("projectName")] public string? ProjectName { get; init; }
+
+    [JsonPropertyName("targetFramework")] public string TargetFramework { get; init; }
+
+    [JsonPropertyName("targetFrameworks")] public string[]? TargetFrameworks { get; init; }
+
+    public ClassLibraryOutputOptions()
+    {
+        Namespace = "Doki.Content";
+        TargetFramework = "net8.0";
+    }
+}
