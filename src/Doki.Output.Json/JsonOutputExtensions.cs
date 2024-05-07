5090b8faf06edc94a6f608de9efcab47c48d66ac
dvolper <github@david.vollmers.org>
2024-05-07 12:13:56 +0200

    json output

---
+++ src/Doki.Output.Json/JsonOutputExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Doki.Output.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Doki.Output.Json;
+
+public static class JsonOutputExtensions
+{
+    [DokiOutputRegistration]
+    public static IServiceCollection AddMarkdownOutput(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddOutputOptions<JsonOutput>("Doki.Output.Json");
+
+        services.AddSingleton<IOutput, JsonOutput>();
+
+        return services;
+    }
+}
