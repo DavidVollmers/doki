b6bd2074727e90fd3c9b928d5bd0125ecd193eb3
dvolper <github@david.vollmers.org>
2024-06-22 14:10:13 +0200

    test version safe nuget loader

---
+++ tests/Doki.CommandLine.Tests/NuGetTests.cs
@@ -0,0 +1,27 @@
+using Doki.CommandLine.NuGet;
+using Doki.Tests.Common;
+using Xunit.Abstractions;
+
+namespace Doki.CommandLine.Tests;
+
+public class NuGetTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public async Task NuGetLoader_TestAsync()
+    {
+        const string packageId = "Doki.Output.Json";
+        var tmpDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+        var logger = new TestOutputLogger(testOutputHelper);
+
+        using var loader = new NuGetLoader(logger);
+
+        await loader.LoadPackageAsync(packageId, tmpDirectory);
+
+        Assert.False(logger.HadError);
+
+        var dllPath = Path.Combine(tmpDirectory, $"{packageId}.1.0.0", "lib", "net8.0", $"{packageId}.dll");
+
+        Assert.True(File.Exists(dllPath));
+    }
+}
